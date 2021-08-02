2fd313809b79ec697b932c6ebd56ffcdda570e93
mati-k <matik.github@gmail.com>
2021-08-03 00:51:20 +0200

    Logging added

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -21,7 +21,7 @@ namespace EMT
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             base.OnStartup(sender, e);
-
+            
             DisplayRootViewFor<ShellViewModel>();
         }
 
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: log4net.Config.XmlConfigurator]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
